887d4d04232ccb4dc72ab99d12659b1eb863dc18
KeinoC <keino.chichester@gmail.com>
2023-10-17 19:30:54 -0400

    adding vercel.json

--- next.config.js
+++ next.config.js
@@ -2,3 +2,15 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+// next.config.js
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+
+module.exports = withPlugins([
+  withImages({
+    // Image configuration options go here, if needed
+  }),
+], {
+  /* Other Next.js configurations go here */
+  // For example, you can configure your assetPrefix, basePath, or other options
+});
