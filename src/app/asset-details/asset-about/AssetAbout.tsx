84e278cc75829bd408c3dea49364f2fcae18cbaa
Chisimdinzotta <108879532+Chisimdinzotta@users.noreply.github.com>
2023-10-22 21:15:44 +0100

    Remove old comments

--- src/app/asset-details/asset-about/AssetAbout.tsx
+++ src/app/asset-details/asset-about/AssetAbout.tsx
@@ -3,11 +3,9 @@ import { assetDetailsContainerClass } from '@/app/utils/styling/tempTWStyles';
 
 
 interface Props {
-    // Define props here
 }
 
 const AssetAbout: React.FC<Props> = (props) => {
-    // Component logic here
 
     return (
         <div className={`${assetDetailsContainerClass}`}>Asset About Container</div>
