09c28ec2b1a2eade6bb55f588691b44d9c44bd90
Chisimdinzotta <108879532+Chisimdinzotta@users.noreply.github.com>
2023-11-18 15:32:04 +0000

    Create latest news component

--- src/app/asset-details/asset-news/AssetNews.tsx
+++ src/app/asset-details/asset-news/AssetNews.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import React, {useEffect, useState} from 'react';
 import { assetDetailsContainerClass } from '@/app/utils/styling/tempTWStyles';
 import { TopBarDataType } from '@/app/utils/seed-data/seed-asset-details';
+import { INews } from '@/app/types/news.type';
+import { newsData } from '@/app/utils/seed-data/seed-news';
 
 interface Props{
     topBarData: TopBarDataType;
@@ -8,11 +12,36 @@ interface Props{
 
 const AssetNews: React.FC<Props> = ({topBarData}) => {
 
+    const [newsDataa, setNewsDataa] = useState <INews[]>(newsData);
+    const firstThreeItems = newsData.slice(0, 3);
+ 
     return (
-        <div className='mb-7 mt-5'>
-            <h1><strong>{topBarData.assetName.toUpperCase()} LATEST NEWS</strong></h1>
-        </div>
-    );
-};
+        <>
+          <h1 className="text-black mb-7 mt-5">
+            <strong>{topBarData.assetName.toUpperCase()} LATEST NEWS</strong>
+          </h1>
+          <div className="mb-7 mt-5 text-black flex flex-nowrap justify-between">
+            {firstThreeItems.map((imageItem, index) => (
+              <div key={index} className="flex flex-col w-1/3 mx-8">
+                <img className="my-2 rounded-lg" src={imageItem.imageURL} alt={imageItem.title} />
+                <h2 className="mb-2">
+                  <strong>{imageItem.title}</strong>
+                </h2>
+                <div className="flex items-center mb-2">
+                  <p className="flex-grow">{imageItem.description.substring(0, 140)}... <a className='text-green-500' href={imageItem.link} target="_blank" rel="noopener noreferrer">
+                    (Read more)
+                  </a></p>
+                </div>
+                <p className="mb-2">
+                  <strong>{imageItem.source}</strong>
+                </p>
+                <p>{imageItem.date}</p>
+              </div>
+            ))}
+          </div>
+          <p>READ MORE</p>
+        </>
+      )};
+      
 
 export default AssetNews;
---
+++ src/app/utils/seed-data/seed-news.tsx
@@ -0,0 +1,31 @@
+import { INews } from "@/app/types/news.type";
+
+export const newsData:INews[] = [
+    {
+        title: "The risking price of gas fee in crypto markets",
+        imageURL: "https://ca.slack-edge.com/T0658TPEHAR-U06625CTH32-fcf46a3966bc-512",
+        description: "Der AfD-Politiker Bernard Keber ist gestorben. Er war Vorstandsmitglied seiner Partei in Marl und sachkundiger Bürger für die AfD-Fraktion im Rat. Der Beitrag AfD Marl trauert um Bernard Keber: Vorstandsmitglied starb überraschend erschien zuerst auf Recklinghäuser Zeitung.",
+        date: "2023-10-24",
+        source: "Forbes",
+        link: "https://www.nrz.de/staedte/essen/alltagshelden/sich-selbst-und-anderen-gutes-tun-id239871709.html",
+    },
+
+    {
+        title: "Solana - is this surge a telling sign?",
+        imageURL: "https://ca.slack-edge.com/T0658TPEHAR-U06625CTH32-fcf46a3966bc-512",
+        description: "Der AfD-Politiker Bernard Keber ist gestorben. Er war Vorstandsmitglied seiner Partei in Marl und sachkundiger Bürger für die AfD-Fraktion im Rat. Der Beitrag AfD Marl trauert um Bernard Keber: Vorstandsmitglied starb überraschend erschien zuerst auf Recklinghäuser Zeitung.",
+        date: "2023-10-24",
+        source: "Crypto Fire",
+        link: "https://www.nrz.de/staedte/essen/alltagshelden/sich-selbst-und-anderen-gutes-tun-id239871709.html",
+    },
+
+    {
+        title: "Latests updates in the European crypto markets",
+        imageURL: "https://ca.slack-edge.com/T0658TPEHAR-U06625CTH32-fcf46a3966bc-512",
+        description: "Der AfD-Politiker Bernard Keber ist gestorben. Er war Vorstandsmitglied seiner Partei in Marl und sachkundiger Bürger für die AfD-Fraktion im Rat. Der Beitrag AfD Marl trauert um Bernard Keber: Vorstandsmitglied starb überraschend erschien zuerst auf Recklinghäuser Zeitung.",
+        date: "2023-10-24",
+        source: "Yahoo News",
+        link: "https://www.nrz.de/staedte/essen/alltagshelden/sich-selbst-und-anderen-gutes-tun-id239871709.html",
+    }
+
+];
